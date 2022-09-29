2eb160b39486fd577e0e0ab5cb9ad1b273af2edd
aspiregabriela <aspiregabriela@gmail.com>
2022-09-29 15:39:59 -0300

    Adicionar arquivos de projeto.

---
+++ AppGuiaCursoDS/AppGuiaCursoDS/Models/Componente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppGuiaCursoDS.Models
+{
+    public class Componente 
+    {
+       public string Nome { get; set; }
+
+       public int CargaHoraria { get; set; }
+
+       public string AtribuicoesResponsabilidades { get; set; }
+
+       public string ValoresAtitudes { get; set; }
+
+       public string Competencias { get; set; }
+
+       public string Habilidades { get; set; }  
+
+       public string BasesTecnologicas { get; set; }
+        
+    }
+}
---
+++ AppGuiaCursoDS/AppGuiaCursoDS/Views/ComponentesPrimeiro.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGuiaCursoDS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ComponentesPrimeiro : ContentPage
+    {
+        public ComponentesPrimeiro()
+        {
+            InitializeComponent();
+    
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+
+                };
+
+            
+            
+            
+            
+            }catch(Exception e)
+            {
+
+            }
+
+
+
+
+        }
+    }
+
+
+
+}
