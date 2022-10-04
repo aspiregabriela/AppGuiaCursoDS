01a6db3f9e6ff481bd0de47a434e20a872d15964
aspiregabriela <aspiregabriela@gmail.com>
2022-10-04 15:48:02 -0300

    Atualização de Componenetes

--- AppGuiaCursoDS/AppGuiaCursoDS/Views/ComponentesPrimeiro.xaml.cs
+++ AppGuiaCursoDS/AppGuiaCursoDS/Views/ComponentesPrimeiro.xaml.cs
@@ -25,24 +25,174 @@ namespace AppGuiaCursoDS.Views
             {
                 var c = new Componente
                 {
+                    Nome ="Linguagem, Trabalho e Tecnologia",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
 
                 };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
 
-            
-            
-            
-            
-            }catch(Exception e)
+            }catch(Exception ex)
             {
+                await DisplaAlert("Ops!", ex.Message, "OK");
+        }
+
+
+
+        private async void Button_Clicked1(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+                    Nome ="Programação e algoritimo",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
+
+                };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
+
+            }catch(Exception ex)
+            {
+                await DisplaAlert("Ops!", ex.Message, "OK");
+           }
+
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+                    Nome ="Linguagem, Trabalho e Tecnologia",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
+
+                };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
+
+            }catch(Exception ex)
+            {
+                await DisplaAlert("Ops!", ex.Message, "OK");
+            }
+
+       private async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+                    Nome ="Linguagem, Trabalho e Tecnologia",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
+
+                };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
+
+            }catch(Exception ex)
+            {
+                await DisplaAlert("Ops!", ex.Message, "OK");
 
             }
 
+        private async void Button_Clicked_4(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+                    Nome ="Linguagem, Trabalho e Tecnologia",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
+
+                };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
+
+            }catch(Exception ex)
+            {
+                await DisplaAlert("Ops!", ex.Message, "OK");
+            }
+
+        private async void Button_Clicked_5(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+                    Nome ="Linguagem, Trabalho e Tecnologia",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
+
+                };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
+
+            }catch(Exception ex)
+            {
+                await DisplaAlert("Ops!", ex.Message, "OK");
+
+            }
+
+        private async void Button_Clicked_6(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+                    Nome ="Linguagem, Trabalho e Tecnologia",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
+
+                };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
+
+            }catch(Exception ex)
+            {
+                await DisplaAlert("Ops!", ex.Message, "OK");
+            }
+
+        private async void Button_Clicked_7(object sender, EventArgs e)
+        {
+            try 
+            {
+                var c = new Componente
+                {
+                    Nome ="Linguagem, Trabalho e Tecnologia",
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulario técnico da área e " +
+                                                    "elaborar resgistros e planilhas de acompanhamento e controle de atividaes",
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução Responsabilizar-se pela produção, utlização e"+
+                                      "divulgação de informações. Estimular a produtividade",
+
+                };
+              await Navigation.PushAsync(new VerComponente(c));  
+            }
+
+            }catch(Exception ex)
+            {
+                await DisplaAlert("Ops!", ex.Message, "OK");
+            }
 
 
-
-        }
-    }
-
-
+       }
 
 }
